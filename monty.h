982b6718a2a16e73bb217002cd2e246042bcfa05
AMR856 <amer.live477@gmail.com>
2023-10-17 11:55:15 +0300

    Fast fix

--- monty.h
+++ monty.h
@@ -6,21 +6,36 @@
 #include <sys/types.h>
 
 /**
- * struct list_t - doubly linked list of a stack
- * @n: an integer data
- * @next: points to the next node in the list or the stack
- * @prev: points to the previous nodein the list or the stack
- * 
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
- * 
-*/
-typedef struct list_t
+ */
+typedef struct stack_s
 {
-    int n;
-    struct stack *next;
-    struct stack *prev;
-} stack;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 
 
 #endif /*MONTY_H*/
